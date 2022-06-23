f08b1157a138fc0b96c78cc3b8797cc1a5f6ba0a
Diana Lupita Garcia <75147104+DianaLuGarcia@users.noreply.github.com>
2022-06-23 11:54:51 -0600

    rollbar

--- server/server.js
+++ server/server.js
@@ -3,6 +3,17 @@ const path = require('path')
 const app = express()
 const port = process.env.PORT || 4005
 
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: '25537b8ed89c41aabe96bc1329a9d45b',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
+
 //middleware same thing as endpoint, you dont need both
 app.use(express.static(path.join(__dirname, '../public')))
 //endpoints
