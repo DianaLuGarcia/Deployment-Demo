f93d0fceffb345dfd07ebbcb12e06613b601077b
Diana Lupita Garcia <75147104+DianaLuGarcia@users.noreply.github.com>
2022-06-22 19:56:25 -0600

    changed package.json

--- server/server.js
+++ server/server.js
@@ -1,17 +1,14 @@
-const express = require('express');
-const cors = require('cors');
-const path = require('path');
+const express = require('express')
+const path = require('path')
 
-const app = express();
-app.use(cors());
-app.use(express.json());
+const app = express()
 
-app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname,"../index.html"))
+app.get('/',function(req,res) {
+  res.sendFile(path.join(__dirname, '../index.html'));
 });
 
-const port = process.env.PORT || 4005;  //if there is no port it will default to 4005
+const port = process.env.PORT || 4005
 
 app.listen(port, () => {
-    console.log(`We vibin on port ${port}`);
+  console.log(`Listening on port ${port}`)
 })
