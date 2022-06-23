2e5fcc14fd2822c62808e76966406a17d74938de
Diana Lupita Garcia <75147104+DianaLuGarcia@users.noreply.github.com>
2022-06-22 20:51:16 -0600

    middlewawre

--- server/server.js
+++ server/server.js
@@ -1,18 +1,19 @@
 const express = require('express')
 const path = require('path')
-
 const app = express()
+const port = process.env.PORT || 4005
 
+//middleware same thing as endpoint, you dont need both
+app.use(express.static(path.join(__dirname, '../public')))
+//endpoints
 app.get('/',function(req,res) {
   res.sendFile(path.join(__dirname, '../public/index.html'));
 });
-
 app.get('/styles',function(req,res) {
     res.sendFile(path.join(__dirname, '../public/index.css'));
   });
+  app.listen(port, () => {
+      console.log(`Listening on port ${port}`)
+    })
 
-const port = process.env.PORT || 4005
 
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`)
-})
